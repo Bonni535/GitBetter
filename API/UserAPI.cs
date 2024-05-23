406a8836a9d7c776eef1938849335946c16a6425
Luca Bonini <luca14bonini@gmail.com>
2024-05-22 17:58:44 -0700

    Api calls for the UserApi file completed

--- API/UserAPI.cs
+++ API/UserAPI.cs
@@ -7,7 +7,58 @@ namespace GitBetter.API
     {
         public static void Map(WebApplication app)
         {
+            //Create a User
+            app.MapPost("users/register", (GitBetterDbContext db, User user) =>
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return Results.Created($"/user/{user.Id}", user);
+            });
 
+            //Get User by Id
+            app.MapGet("/users/{id}", (GitBetterDbContext db, int id) =>
+            {
+               var user = db.Users.FirstOrDefault(u => u.Id == id);
+
+                if (user == null)
+                {
+                    return Results.NotFound();
+                }
+
+                return Results.Ok(user);
+            });
+
+            //Update a User
+            app.MapPut("users/{id}", (GitBetterDbContext db, int id, User users) =>
+            {
+                DateTime changeDate = DateTime.Now;
+                User updateUser = db.Users.SingleOrDefault(u => u.Id == id);
+                if (updateUser == null)
+                {
+                    return Results.NotFound("User not found.");
+                }
+                updateUser.Id = users.Id;
+                updateUser.Name = users.Name;
+                updateUser.Email = users.Email;
+
+
+                db.SaveChanges();
+                return Results.Ok(users);
+            });
+
+            //Delete a User
+            app.MapDelete("/users/{id}", (GitBetterDbContext db, int id) =>
+            {
+                var userToDelete = db.Users.Include(u => u.Appointments).FirstOrDefault(u => u.Id == id);
+                if (userToDelete == null)
+                {
+                    return Results.NotFound("No User was found.");
+                }
+
+                db.Users.Remove(userToDelete);
+                db.SaveChanges();
+                return Results.Ok(db.Users);
+            });
         }
     }
 }
